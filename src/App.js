07192032965d404939df0937e2a8d04806a4094c
nickbrimmer <nickbrimmer@gmail.com>
2022-08-26 19:03:51 -0700

    CC20-1000 this.setState()

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
+import { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // local state
+  constructor(){ // available on all class components, when "constructed"
+    super();  // calls any other underlying classes you are calling with your method
+    
+    // this is referencing THIS class
+    // JavaScript knows a change by referencing a new memory object
+    // Object.assign({}, obj1)
+    this.state = {
+      name: 'Nick',
+      company: "Emsi"  // all React cares about is that the key matches, and will just overwrite the value
+    }
+  }
+
+  // this is what you are RENDERING
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={ logo } className="App-logo" alt="logo" />
+          <p>
+            Hello {this.state.name}, I work at {this.state.company}
+          </p>
+          {/* setting state to a NEW object -- SHALLOW MERGE */}
+          <button onClick={() => {
+            this.setState({name: "Joey"}) // state updated Asynchronously!!!
+            // console.log(this.state) 
+            // this.setState(() => {}, () => {})  (updater function, )
+            this.setState((state, props) => {
+              return {
+                name: "Gabe"
+              }
+            }, () => {
+              // optional second callback - "call back when the previous function is fully done"
+              // this function is going to run synchronously within the closure.
+              console.log(this.state)
+            }) 
+          }}>Change my name</button>
+          
+        </header>
+      </div> 
+    );
+  }
 }
 
 export default App;
