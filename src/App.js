95cca665360a777a261235162c6bed18340d0ccb
nickbrimmer <nickbrimmer@gmail.com>
2022-08-27 11:57:10 -0700

    CC20-1001 searchField addition

--- src/App.js
+++ src/App.js
@@ -1,35 +1,48 @@
-import { Component } from 'react'
+import { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor(){
-    super(); 
-  
+  constructor() {
+    super();
+
     this.state = {
-      monsters: []
-    }
+      monsters: [],
+      searchField: ''
+    };
   }
 
-  componentDidMount(){
+  componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState(() => {
-      return { monsters: users}
-    },
-    () => {
-      console.log(this.state)
-    }))
+      .then(response => response.json())
+      .then(users =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
   }
 
-render() {
+  render() {
+    const filteredMonsters = this.state.monsters.filter(monster => {
+      return monster.name.toLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
-          {
-            this.state.monsters.map((monster) => {
-              return <h1 key={monster.name}>{monster.name}</h1>
-            })
-          }
-      </div> 
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search monsters"
+          onChange={event => {
+            const searchField = event.target.value.toLowerCase();
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
+        {filteredMonsters.map(monster => {
+          return <h1 key={monster.name}>{monster.name}</h1>;
+        })}
+      </div>
     );
   }
 }
